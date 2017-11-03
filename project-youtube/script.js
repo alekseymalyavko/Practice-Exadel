bb579fb102696a79a5cf66c6a732242a6b9658e5
Aexey <aleksmalyavko@gmail.com>
2017-11-03 15:02:07 +0200

    add videos

--- project-youtube/script.js
+++ project-youtube/script.js
@@ -1,110 +1,204 @@
-
-
-
-
 (function() {
 
+    var searchtext = "";
+    var videoItems = [];
 
-/*header*/
+
+
+
+    /*header*/
     var header = document.createElement("header");
     document.body.appendChild(header);
 
-function addLogo() {
-    var logo = document.createElement("img");
-    logo.src = "yt.png";
-    logo.id = "logo";
-    header.appendChild(logo);
-}
-    
-function addInput() {
-    
-    var input = document.createElement("input");
-    input.placeholder = "Search on YouTube";
-    input.type = "search";
-    input.id = "search";
-    header.appendChild(input);
+    function addLogo() {
+        var logo = document.createElement("img");
+        logo.src = "yt.png";
+        logo.id = "logo";
 
-};
+        header.appendChild(logo);
+    }
 
-/*header*/
+    function addInput() {
 
-/*body*/
+        var input = document.createElement("input");
+        input.placeholder = "Search on YouTube";
+        input.type = "search";
+        input.id = "search";
+        input.onkeypress = function(e) {
+            e = e || window.event;
+            if (e.keyCode === 13) {
+                searchtext = e.target.value;
 
-    section = document.createElement("section");
-    section.id = "main";
-    document.body.appendChild(section);
+                searching();
+                emptyList();
 
+            }
+        }
+        header.appendChild(input);
 
-
-/*search-result*/
-
-function addSresults(){
-
-    var section_search = document.createElement("section");
-    section_search.className = "second";
-    section.appendChild(section_search);
+    };
 
 
-    var first_div = document.createElement("div");
-    first_div.className="img-des";
-    section_search.appendChild(first_div);
+    function searching() {
+        var xhr = new XMLHttpRequest(searchtext);
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+
+
+                var info = JSON.parse(xhr.response);
+
+                console.log(info);
+                var info1 = Object.values(info.items);
+
+                if (info1) {
+                    initClipList(info1);
+                }
+            }
+        }
+        xhr.open('GET', 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key=AIzaSyC3nnobkH5nwPBr52O9zHKK2ZWgQbQT86A&q=' + searchtext, true);
+        xhr.send();
+
+    };
+
+    function initClipList(items) {
+
+        var clipList = [];
+
+        for (var i = 0; i < items.length; i++) {
+
+            var entry = items[i];
+            var date = new Date(Date.parse(entry.snippet.publishedAt));
+            var shortId = entry.id.videoId;
+
+
+            clipList.push({
+                id: shortId,
+                youtubeLink: "http://www.youtube.com/watch?v=" + shortId,
+                title: entry.snippet.title,
+                thumbnail: entry.snippet.thumbnails.high.url,
+                description: entry.snippet.description,
+                author: entry.snippet.channelTitle,
+                // viewCount: entry.brandingSettings.trackingImageUrl,
+                publishDate: (date.getMonth() + 1) + "." + date.getDate() + "." + date.getFullYear()
+            });
+        }
+
+        videoItems = clipList;
+
+        console.log(videoItems);
+
+
+        if (videoItems) {
+            addSresults(videoItems);
+        }
+    }
 
 
 
-    var img = document.createElement("img");
-    img.id="img";
-    img.src="https://content.rspca.org.uk/cmsprd/Satellite?blobcol=urldata&blobkey=id&blobtable=MungoBlobs&blobwhere=1232999396412&ssbinary=true";
-    first_div.appendChild(img);
 
+    /*header*/
 
-
-    var a = document.createElement("a");
-    a.id="description_link";
-    a.innerHTML = "<h1>"+"Dangerous rabbits"+"</h1>";
-    a.href = "";
-    first_div.appendChild(a);
-
-
-
-    var p = document.createElement('div');
-    p.id="description_text";
-    p.innerHTML= "<p>"+"This video about rabbits which killed people."+"</p>"+"<p>"+"Author:"+"Ivan Petrov"+"</p>";
-    first_div.appendChild(p);
-
-
-
-
-    var info_div = document.createElement("div");
-    info_div.id="description_stats";
-    info_div.innerHTML = "<span>"+"Views:"+"53 000 000"+"</span>"+"<span>"+"Date:"+"21.08.2021"+"</span>";
-    first_div.appendChild(info_div);        
-
-};
-
-/*search-result*/
+    /*body*/
 
 
 
 
 
+    /*search-result*/
 
-/*body*/
+    function addSresults(items) {
+
+        section = document.createElement("section");
+        section.id = "main";
+        document.body.appendChild(section);
+
+        var section_search = document.createElement("section");
+        section_search.id = "second";
+        section.appendChild(section_search);
 
 
-/*footer*/
-function addFooter(){
-    footer = document.createElement("footer");
-    document.body.appendChild(footer);
 
-    drop=document.createElement('a')
-    footer.appendChild(drop);
-}
+        for (var i = 0; i < items.length; i++) {
 
-/*footer*/
+            var videoItems = items[i];
 
-addLogo();
-addInput();
-addSresults();
-addFooter();
+
+
+            var first_div = document.createElement("div");
+            first_div.className = "img-des";
+            section_search.appendChild(first_div);
+
+
+
+            var img = document.createElement("img");
+            img.id = "img";
+            img.src = videoItems.thumbnail;
+            first_div.appendChild(img);
+
+
+
+            var a = document.createElement("a");
+            a.id = "description_link";
+            a.innerHTML = "<h1>" + videoItems.title + "</h1>";
+            a.href = videoItems.youtubeLink;
+            first_div.appendChild(a);
+
+
+
+            var p = document.createElement('div');
+            p.id = "description_text";
+            p.innerHTML = "<p>" + videoItems.description + "</p>" + "<p>" + "Author:" + videoItems.author + "</p>";
+            first_div.appendChild(p);
+
+
+
+
+            var info_div = document.createElement("div");
+            info_div.id = "description_stats";
+            info_div.innerHTML = "<span>" + "Views:" + videoItems.viewCount + "</span>" + "<span>" + "Date:" + videoItems.publishDate + "</span>";
+            first_div.appendChild(info_div);
+
+        }
+
+
+    };
+
+
+    function emptyList() {
+
+        document.body.removeChild(section);
+
+    }
+
+
+
+
+
+    /*search-result*/
+
+
+
+
+
+
+    /*body*/
+
+
+    /*footer*/
+    function addFooter() {
+        footer = document.createElement("footer");
+        document.body.appendChild(footer);
+
+        drop = document.createElement('a')
+        footer.appendChild(drop);
+    }
+
+    /*footer*/
+
+    addLogo();
+    addInput();
+    // addSresults();
+    addFooter();
 
 })();
